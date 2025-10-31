225373e7911dab811f4b66502b971eda2cc2d6f2
davemfish <davemfish@gmail.com>
2025-10-31 14:07:24 -0400

    cleanup cv python script

--- src/invest_reports/jinja_report_generators/cv_report_generator.py
+++ src/invest_reports/jinja_report_generators/cv_report_generator.py
@@ -5,15 +5,12 @@ import sys
 import time
 
 import altair
-import geometamaker
 import geopandas
 import pandas
 from jinja2 import Environment, PackageLoader, select_autoescape
 import natcap.invest.utils
 import natcap.invest.datastack
-from natcap.invest.coastal_vulnerability import MODEL_SPEC
 
-from invest_reports.utils import RasterPlotConfig
 
 LOGGER = logging.getLogger(__name__)
 
@@ -30,10 +27,6 @@ env = Environment(
     autoescape=select_autoescape()
 )
 template = env.get_template('coastal_vulnerability.html')
-
-# Define basic info.
-model_name = 'Coastal Vulnerability'
-timestamp = time.strftime('%Y-%m-%d %H:%M')
 
 stroke_width = 0.75
 # When points are low-density, fill is nicer, or a thicker stroke.
@@ -102,8 +95,8 @@ def chart_base_points(geodataframe):
     return base_points
 
 
-def chart_wave_energy(wave_energy_geo, energy_units):
-    base_points = chart_base_points(wave_energy_geo)
+def chart_wave_energy(wave_energy_geodf, energy_units):
+    base_points = chart_base_points(wave_energy_geodf)
 
     points = base_points.mark_circle(
         filled=point_fill,
@@ -121,9 +114,9 @@ def chart_wave_energy(wave_energy_geo, energy_units):
     return points
 
 
-def chart_habitat_map(habitat_protection_csv, exposure_geo, landmass):
+def chart_habitat_map(habitat_protection_csv, exposure_geodf, landmass_chart):
     habitat_df = pandas.read_csv(habitat_protection_csv)
-    habitat_geo = exposure_geo[['shore_id', 'geometry', 'habitat_role']].join(
+    habitat_geodf = exposure_geodf[['shore_id', 'geometry', 'habitat_role']].join(
         habitat_df.set_index('shore_id'), on='shore_id')
     habitats = set(habitat_df.columns).difference(set(['shore_id', 'R_hab']))
 
@@ -133,7 +126,7 @@ def chart_habitat_map(habitat_protection_csv, exposure_geo, landmass):
             if row[h] != 5:
                 hab_list.append(h)
         return ','.join(hab_list)
-    habitat_geo['hab_presence'] = habitat_geo.apply(concat_habitats, axis=1)
+    habitat_geodf['hab_presence'] = habitat_geodf.apply(concat_habitats, axis=1)
 
     habitat_radio = altair.binding_radio(
         options=['All'] + list(habitats),
@@ -142,11 +135,7 @@ def chart_habitat_map(habitat_protection_csv, exposure_geo, landmass):
     )
     hab_param = altair.param(value='All', bind=habitat_radio)
 
-    # TODO: can all maps reference the same json point dataset to save space?
-    # Charts that are concatenated into the same specification will. Charts spread
-    # across specs will not. Perhaps charts spread across specs could, but it
-    # probably requires ditching altair and constructing the vegalite specs another way.
-    habitat_base_points = chart_base_points(habitat_geo)
+    habitat_base_points = chart_base_points(habitat_geodf)
 
     habitat_points = habitat_base_points.add_params(
         hab_param
@@ -162,8 +151,8 @@ def chart_habitat_map(habitat_protection_csv, exposure_geo, landmass):
         tooltip=altair.Tooltip(list(habitats), format='.2f')
     )
 
-    _, xy_ratio = get_geojson_bbox(exposure_geo)
-    habitat_map = landmass + habitat_points
+    _, xy_ratio = get_geojson_bbox(exposure_geodf)
+    habitat_map = landmass_chart + habitat_points
     habitat_map = habitat_map.properties(
         width=map_width,
         height=map_width / xy_ratio,
@@ -193,13 +182,15 @@ def report(logfile_path, model_spec):
     landmass_geo = geopandas.read_file(
         file_registry['clipped_projected_landmass'])
     extent_feature, xy_ratio = get_geojson_bbox(exposure_geo)
-    landmass = chart_landmass(
+
+    landmass_chart = chart_landmass(
         landmass_geo, clip=True, extent_feature=extent_feature)
     base_points = chart_base_points(exposure_geo)
     
     tooltip_vars = ['exposure'] + rank_vars
     if 'R_geomorph' in exposure_geo:
         rank_vars.append('R_geomorph')
+
     scale_population = altair.param(value=False)
     if 'population' in exposure_geo:
         # Population is used to scale point size in exposure maps,
@@ -219,7 +210,7 @@ def report(logfile_path, model_spec):
         'population:Q',
         altair.value(point_size))
 
-    exposure_points = base_points.transform_filter(
+    exposure_points_chart = base_points.transform_filter(
         altair.expr.isValid(altair.datum.exposure)
     ).mark_circle(
         filled=point_fill,
@@ -233,7 +224,7 @@ def report(logfile_path, model_spec):
         tooltip=tooltip
     ).add_params(scale_population)
 
-    _null_points = base_points.add_params(
+    null_points_chart = base_points.add_params(
         show_null
     ).transform_filter(
         show_null & ~altair.expr.isValid(altair.datum.exposure)
@@ -247,7 +238,7 @@ def report(logfile_path, model_spec):
         tooltip=tooltip
     ).add_params(scale_population)
 
-    exposure_map = landmass + _null_points + exposure_points
+    exposure_map = landmass_chart + null_points_chart + exposure_points_chart
     exposure_map = exposure_map.properties(
         width=map_width,
         height=map_width / xy_ratio,
@@ -258,7 +249,7 @@ def report(logfile_path, model_spec):
     habitat_map = chart_habitat_map(
         file_registry['habitat_protection'],
         exposure_geo,
-        landmass)
+        landmass_chart)
     habitat_map_json = habitat_map.to_json()
 
     habitat_params_df = pandas.read_csv(args_dict['habitat_table_path'])
@@ -278,27 +269,27 @@ def report(logfile_path, model_spec):
     )
     exposure_histogram_json = exposure_histogram.to_json()
 
-    base_rank_vars_spec = base_points.mark_circle(
+    base_rank_vars_chart = base_points.mark_circle(
         filled=point_fill,
         strokeWidth=stroke_width,
         size=point_size
     )
-    rank_vars_spec_list = []
+    rank_vars_chart_list = []
     for var in rank_vars:
-        point_spec = base_rank_vars_spec.encode(
+        point_chart = base_rank_vars_chart.encode(
             color=altair.Color(
                 f'{var}:Q',
                 legend=altair.Legend(title='rank')
             ).scale(scheme='plasma', reverse=True),
         )
-        rank_vars_spec_list.append(
-            altair.layer(landmass, point_spec).properties(
+        rank_vars_chart_list.append(
+            altair.layer(landmass_chart, point_chart).properties(
                 title=var))
     
     n_cols = len(rank_vars) // 2
     rank_vars_figure = altair.vconcat(
-        altair.hconcat(*rank_vars_spec_list[:n_cols]),
-        altair.hconcat(*rank_vars_spec_list[n_cols:])
+        altair.hconcat(*rank_vars_chart_list[:n_cols]),
+        altair.hconcat(*rank_vars_chart_list[n_cols:])
     )
     rank_vars_figure_json = rank_vars_figure.to_json()
 
@@ -327,8 +318,9 @@ def report(logfile_path, model_spec):
     energy_units = 'kW/meter'
     wave_energy_geo = wave_energy_geo.join(
         intermediate_df[['shore_id', 'wave']].set_index('shore_id'), on='shore_id')
-    wave_points = chart_wave_energy(wave_energy_geo, energy_units)
-    wave_energy_map = landmass + wave_points
+
+    wave_points_chart = chart_wave_energy(wave_energy_geo, energy_units)
+    wave_energy_map = landmass_chart + wave_points_chart
     wave_energy_map = wave_energy_map.properties(
         width=map_width + 30,  # extra space for legend
         height=map_width / xy_ratio,
@@ -336,13 +328,7 @@ def report(logfile_path, model_spec):
     ).configure_legend(**legend_config)
     wave_energy_map_json = wave_energy_map.to_json()
 
-    # # Plot inputs.
-    # inputs_img_src = invest_reports.utils.plot_and_base64_encode_rasters([
-    #     RasterPlotConfig(args_dict['dem_path'], 'continuous'),
-    #     RasterPlotConfig(args_dict['runoff_proxy_path'], 'continuous'),
-    #     RasterPlotConfig(args_dict['lulc_path'], 'nominal')
-    # ])
-
+    # later this may be in model_spec
     model_description = \
         """
         The Coastal Vulnerability model calculates a coastal exposure index,
@@ -353,15 +339,17 @@ def report(logfile_path, model_spec):
         variables.
         """
     # Generate HTML document.
+    model_name = model_spec.model_title
     report_filename = os.path.join(
         workspace, f'{model_name.lower()}{args_dict['results_suffix']}.html')
     with open(report_filename, 'w', encoding='utf-8') as target_file:
         target_file.write(template.render(
             report_script=__file__,
-            timestamp=timestamp,
+            timestamp=time.strftime('%Y-%m-%d %H:%M'),
             page_title=f'InVEST Results: {model_name}',
             model_name=model_name,
-            model_description=model_description,  # later this may in model_spec
+            model_description=model_description,
+            userguide_page=model_spec.userguide,
             args_dict=args_dict,
             exposure_map_json=exposure_map_json,
             habitat_map_json=habitat_map_json,
@@ -371,12 +359,14 @@ def report(logfile_path, model_spec):
             facetted_histograms_json=facetted_histograms_json,
             rank_vars_figure_json=rank_vars_figure_json,
             wave_energy_map_json=wave_energy_map_json,
+            model_spec_outputs=model_spec.outputs,
             accordions_open_on_load=True,
         ))
     LOGGER.info(f'Created {report_filename}')
 
 
 if __name__ == '__main__':
+    from natcap.invest.coastal_vulnerability import MODEL_SPEC
     handler = logging.StreamHandler(sys.stdout)
     logging.basicConfig(level=logging.INFO, handlers=[handler])
     # logfile_path = 'C:/Users/dmf/projects/forum/cv/mar/sample_200m_12k_fetch/InVEST-coastal_vulnerability-log-2025-10-03--11_55_19.txt'
