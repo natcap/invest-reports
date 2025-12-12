f1955cfcf258d84f51f96cda90d6463e705a80d6
Emily Davis <eadavis@stanford.edu>
2025-12-11 18:48:49 -0700

    Update SDR tests with truthy values and comments.

--- tests/test_sdr.py
+++ tests/test_sdr.py
@@ -13,22 +13,30 @@ class SDRReportTests(unittest.TestCase):
     def test_render(self):
         """Basic test to make sure the template renders without error."""
 
+        args_dict = {'suffix': 'test'}
+        img_src = 'bAse64eNcoDEdIMagE'
+        ws_vector_table = '<table class="test__results-table></table>'
+        ws_vector_totals_table = '<table class="test__totals-table"></table>'
+        output_stats_table = '<table class="test__output-stats-table></table>'
+        input_stats_table = '<table class="test__input-stats-table></table>'
+        stats_table_note = 'This is a test!'
+
         html = TEMPLATE.render(
             report_script=__file__,
             model_id=MODEL_SPEC.model_id,
             model_name=MODEL_SPEC.model_title,
             userguide_page=MODEL_SPEC.userguide,
-            timestamp='',
-            args_dict={},
-            inputs_img_src='',
-            outputs_img_src='',
+            timestamp='1970-01-01',
+            args_dict=args_dict,
+            inputs_img_src=img_src,
+            outputs_img_src=img_src,
             intermediate_outputs_heading='Stream Network Maps',
-            intermediate_outputs_img_src='',
-            ws_vector_table='',
-            ws_vector_totals_table='',
-            output_raster_stats_table='',
-            input_raster_stats_table='',
-            stats_table_note='',
+            intermediate_outputs_img_src=img_src,
+            ws_vector_table=ws_vector_table,
+            ws_vector_totals_table=ws_vector_totals_table,
+            output_raster_stats_table=output_stats_table,
+            input_raster_stats_table=input_stats_table,
+            stats_table_note=stats_table_note,
             model_spec_outputs=MODEL_SPEC.outputs,
             accordions_open_on_load=True,
         )
@@ -71,11 +79,14 @@ class SDRReportTests(unittest.TestCase):
 
         root = lxml.html.document_fromstring(html)
 
-        tables = root.findall('.//table')
-        self.assertEqual(len(tables), 2)
-
         totals_table = root.find_class('test__totals-table')
         self.assertTrue(len(totals_table) == 1)
+
+        # Ideally, this test would select only the tables in the 'Results by
+        # Watershed' section, but this seems impossible without cumbersome
+        # tree traversal that would likely result in a brittle test.
+        tables = root.findall('.//table')
+        self.assertEqual(len(tables), 2)
 
     def test_watershed_results_without_totals(self):
         """Totals should be not be rendered when there are none to render."""
@@ -107,6 +118,8 @@ class SDRReportTests(unittest.TestCase):
 
         root = lxml.html.document_fromstring(html)
 
-        # Make sure there is only 1 table.
+        # Ideally, this test would select only the tables in the 'Results by
+        # Watershed' section, but this seems impossible without cumbersome
+        # tree traversal that would likely result in a brittle test.
         tables = root.findall('.//table')
         self.assertEqual(len(tables), 1)
